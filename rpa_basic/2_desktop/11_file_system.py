919eae87d04fd6f4f0242f2a6691cc594a029de8
prolkh <prolkh@gmail.com>
2021-01-18 22:31:49 +0900

    테스트 : rpa 데탑 자동화 파일 시스템을 이용해라2
    파일 만들기
    파일명 변경하기
    파일 삭제하기
    폴더 만들기
    폴더명 변경하기
    폴더 지우기
    파일 복사하기
    폴더 복사하기
    폴더 이동하기

--- rpa_basic/2_desktop/11_file_system.py
+++ rpa_basic/2_desktop/11_file_system.py
@@ -69,3 +69,62 @@ for root, dirs, files, in os.walk(os.getcwd()):
         if fnmatch.fnmatch(name, pattern): # 이름과 패턴이 일치하면
             result.append(os.path.join(root, name))
 print(result)
+
+
+# 주어진 경로가 폴더인지 파일인지
+isdir = os.path.isdir("python_study")
+print(isdir) # 폴더면 True
+
+isfile = os.path.isfile("sample.xlsx")
+print(isfile) # 파일이면 True
+
+print(os.path.isfile("babo.png")) # 없으면 False
+
+# 주어진 경로가 존재하는지
+exists = os.path.exists("sample.xlsx")
+print(exists) # 존재하면 True
+
+# 파일 만들기
+# open("new_file.txt", "a").close() # 빈 파일 생성
+
+# 파일명 변경하기
+# os.rename("new_file.txt", "new_file_rename.txt") # new_file.txt -> new_file_rename.txt
+
+# 파일 삭제하기
+# os.remove("new_file_rename.txt")
+
+# 폴더 만들기
+# os.mkdir("new_folder") # 현재 경로 기준으로 폴더 생성
+# os.mkdir("d:/python_study/coding") # 절대 경로 기준으로 폴더 생성
+
+# os.mkdir("new_file/a/b/c") # 실패 : 하위 폴더를 가지는 폴더 생성 시도
+# os.mkdirs("new_filse/a/b/c") # 성공: 하위 폴더를 가지는 폴더 생성
+
+# 폴더명 변경하기
+# os.rename("new_folder", "new_folder_rename")
+
+# 폴더 지우기
+# os.rmdir("new_folders") # 폴더 안이 비었을 때만 삭제 가능
+
+import shutil # shell utilities
+# shutil.rmtree("new_folders") # 폴더 안이 비어 있지 않아도 완전 삭제 가능
+# 모든 파일이 삭제될 수 있으므로 주의 !!
+
+# 파일 복사하기
+# 어떤 파일을 폴더 안으로 복사하기
+# shutil.copy("run_btn.png", "test_folder") # 원본 파일 경로, 대상 폴더 경로
+
+# 파일을 폴더 안에 새로은 이름으로 복사하기
+# shutil.copy("run_btn.png", "test_folder/copy.png") # 원본 파일 경로, 대상 폴더 경로(변경된 파일명까지)
+# shutil.copyfile("run_btn.png", "test_folder/copyfile.png") # 원본 파일 경로, 대상 폴더 경로(변경된 파일명까지)
+
+# shutil.copy2("run_btn.png", "test_folder/copy2.png") # 원본 파일 경로, 대상 폴더 경고(파일 명까지)
+
+# copy, copyfile : 메타정보 복사x
+# copy2 : 메타정보 복사x
+
+# 폴더 복사
+# shutil.copytree("folder", "copytree") # 원본 폴더 경로, 대상 폴더 경로
+
+# 폴더 이동(이동할 폴더가 없으면 폴더명이 변경됨)
+# shutil.move("folder", "move")
