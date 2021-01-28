4a4f5e1cb361683d3389afe2bbf450cbd6e816a5
prolkh <prolkh@gmail.com>
2021-01-28 23:56:14 +0900

    테스트 : rpa 웹 자동화 동적 페이지 스크롤

--- rpa_basic/3_web/06_page_scroll.py
+++ rpa_basic/3_web/06_page_scroll.py
@@ -17,7 +17,31 @@ elem.send_keys(Keys.ENTER)
 # browser.execute_script('window.scrollTo(0, 1080)')
 
 # 화면 가장 아래로 스크롤 내리기
-browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+# browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+
+# 동적 페이지 스크롤 내리기
+interval = 1
+
+prev_height = browser.execute_script('return document.body.scrollHeight')
+
+# 반복 수행
+while True:
+    # 스크롤을 화면 가장 아래로 내림
+    browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+
+    # 페이지 로딩 대기
+    time.sleep(interval)
+
+    # 현재 문서 높이 가져와서 저장
+    curr_height = browser.execute_script('return document.body.scrollHeight')
+    if curr_height == prev_height: # 끝이면 탈출
+        break
+
+    prev_height = curr_height
+
+# 맨 위로 올리기
+browser.execute_script('window.scrollTo(0, 0)')
+
 
 time.sleep(5)
 browser.quit()
